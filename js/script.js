0ba5cf72f05df19028b03a587bc6aa34c05b8ce0
Kazim Shabbir <Kazimshabbir2010@hotmail.com>
2020-02-25 14:31:16 -0600

    created clickable link

---
+++ js/script.js
@@ -0,0 +1,15 @@
+
+function Khabib() {
+document.getElementById("character-box").style.backgroundImage = "url(https://media.gettyimages.com/photos/khabib-nurmagomedov-of-russia-poses-for-a-portrait-backstage-after-picture-id943251436?s=612x612)";
+var p = document.createElement("p")
+
+p.innerHTML= 
+    `Age 31 Gender Male\n
+    Height	5 ft 11 inches\n
+    Weight Class: Lightweight\n
+    Out of Dagestan\n
+    Khabib Nurmagomedov is a MMA fighter with a professional fight record of 28 wins, 0 losses and 0 draws`;
+    p.setAttribute("class", "Khabibinfo")
+    document.getElementById("character-box").append(p)
+
+}
